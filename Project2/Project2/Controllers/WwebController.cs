c0d14051b504ca81a4b9bdcd58f9221986db628f
kmssujith <62741661+kmssujith@users.noreply.github.com>
2021-08-27 00:43:07 -0700

    Add files via upload

---
+++ Project2/Project2/Controllers/WwebController.cs
@@ -0,0 +1,24 @@
+﻿using Hangfire.MemoryStorage.Dto;
+using MaheApi.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nancy.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project2.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WwebController : ControllerBase
+    {
+        public List<String> StudentIndex()
+        {
+            var responseString = ApiCall.GetApi("https://localhost:44302/Web");
+            var rootobject = new JavaScriptSerializer().Deserialize<List<String>>((string)responseString);
+            return rootobject;
+        }
+    }
+}
