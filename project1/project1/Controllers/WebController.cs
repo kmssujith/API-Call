e6ffa82beec860bca3c3ef9403056b5a65e8c5c0
kmssujith <62741661+kmssujith@users.noreply.github.com>
2021-08-27 00:44:24 -0700

    Add files via upload

---
+++ project1/project1/Controllers/WebController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project1.Controllers
+{
+    
+    [Route("[controller]")]
+    public class WebController : ControllerBase
+    {
+        private static List<string> studentnames = new List<string> { "A", "B", "C", "D" };
+
+        public static List<string> Studentnames { get => studentnames; set => studentnames = value; }
+        [HttpGet]
+        public IEnumerable<string> GetName()
+        {                                                                 
+
+            return Studentnames;
+
+        }
+    }
+}
